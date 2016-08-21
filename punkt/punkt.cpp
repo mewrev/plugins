7f25100d087e36fd39bca7ed81049b8de9a7f24d
mewmew <rnd0x00@gmail.com>
2016-08-21 15:08:13 +0200

    punkt: Generate dedicated temp output directory for DOT files.

--- punkt/punkt.cpp
+++ punkt/punkt.cpp
@@ -1,11 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <idp.hpp>
 #include <loader.hpp>
 #include <gdl.hpp>
 
 // dump_cfgs dumps the control flow graph of each function contained within the
-// executable and stores them as DOT files within the /tmp directory.
+// executable and stores them as DOT files within a /tmp/punkt.XXXXXX directory.
 void dump_cfgs() {
+	char pattern[] = "/tmp/punkt.XXXXXX";
+	char *dir = mkdtemp(pattern);
+	if (dir == NULL) {
+		warning("unable to create output directory `%s`", dir);
+		return;
+	}
 	int nfuncs = get_func_qty();
 	for (int i = 0; i < nfuncs; i++) {
 		func_t *f = getn_func(i);
@@ -13,12 +20,13 @@ void dump_cfgs() {
 		char title[32];
 		qsnprintf(title, sizeof(title), "%08X", faddr);
 		char dst_path[1024];
-		qsnprintf(dst_path, sizeof(dst_path), "/tmp/%08X.dot", faddr);
+		qsnprintf(dst_path, sizeof(dst_path), "%s/%08X.dot", dir, faddr);
 		if (!gen_flow_graph(dst_path, title, f, 0, 0, CHART_GEN_DOT)) {
 			warning("unable to generate CFG for function at address %08X", faddr);
 			continue;
 		}
 	}
+	warning("punkt completed successfully :) DOT files stored in `%s`", dir);
 }
 
 int idaapi init(void) {
@@ -27,7 +35,6 @@ int idaapi init(void) {
 
 void idaapi run(int) {
 	dump_cfgs();
-	warning("punkt completed successfully :) DOT files stored in /tmp/");
 }
 
 plugin_t PLUGIN = {
